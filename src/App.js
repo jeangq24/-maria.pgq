d2d51320e34f7b5e3238f39eb26bf5aa931298bf
Jean <jeangarzon24@gmail.com>
2022-03-01 19:17:49 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-
+import TajetaPrinicipal from './components/tarjetaPrincipal.jsx';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span className="absolute text-xs font-Roboto-serif font-bold">Tarjeta realizada por <a className="text-sm bg-blue-50 pt-1 hover:bg-blue-200 rounded-md" href='https://www.linkedin.com/in/jeangq24/' target={"_blank"}>@JEAN GARZÓN</a></span>
+      <TajetaPrinicipal/>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Roboto-serif': ["Roboto Serif", "sans-serif"],
+        'Roboto-mono' :['Roboto Mono', 'monospace']
+      }
+    },
+  },
+  plugins: [],
+}
