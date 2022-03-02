458738a73cd77aa336f0ce38efbfddc6cd469f9d
Jean <jeangarzon24@gmail.com>
2022-03-02 15:50:01 -0500

    add animation

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import TajetaPrinicipal from './components/tarjetaPrincipal.jsx';
 function App() {
   return (
     <div className="App">
-      <span className="absolute text-xs font-Roboto-serif font-bold">Tarjeta realizada por <a className="text-sm bg-blue-50 pt-1 hover:bg-blue-200 rounded-md" href='https://www.linkedin.com/in/jeangq24/' target={"_blank"}>@JEAN GARZÓN</a></span>
+      <span className="absolute text-xs font-Roboto-serif font-bold mt-1">Tarjeta realizada por <button className="animate-bounce"><a className="text-sm bg-blue-50 pt-1 hover:bg-blue-200 rounded-md" href='https://www.linkedin.com/in/jeangq24/' target={"_blank"}>@JEAN GARZÓN</a></button></span>
       <TajetaPrinicipal/>
     </div>
   );
--- src/components/tarjetaPrincipal.jsx
+++ src/components/tarjetaPrincipal.jsx
@@ -1,5 +1,5 @@
-import {AiFillInstagram} from 'react-icons/ai';
-import {SiTiktok} from 'react-icons/si'
+import { AiFillInstagram } from 'react-icons/ai';
+import { SiTiktok } from 'react-icons/si'
 export default () => {
     return (
         <div className="filtro">
@@ -9,10 +9,12 @@ export default () => {
                 <span className="nombre-perfil">María Paula Garzón</span>
                 <span className="descripcion">Make-up | Outfits</span>
                 <div className="contenedor-botones">
-                    <button className="btn-redes mb-2"><AiFillInstagram size={60} onClick={()=>{ window.open('https://instagram.com/maria.pgq?utm_medium=copy_link', '_blank')}}/></button>
-                    <button className='btn-redes mt-2 py-1'><SiTiktok size={50} onClick={()=>{
-                        window.open('https://vm.tiktok.com/ZMLA2NCc8/', '_blank');  
-                    }}/></button>
+                    <button className="btn-redes mb-2"><AiFillInstagram size={50} onClick={() => { window.open('https://instagram.com/maria.pgq?utm_medium=copy_link', '_blank') }} />
+                    </button>
+
+                    <button className='btn-redes mt-2 py-1'><SiTiktok size={40} onClick={() => {
+                        window.open('https://vm.tiktok.com/ZMLA2NCc8/', '_blank');
+                    }} /></button>
                 </div>
             </div>
         </div>
